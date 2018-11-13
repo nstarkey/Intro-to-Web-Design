7e1a5edebf3b2132ab6f0bc2c32112bd679eb74f
Nicole Starkey <coliestarkey@Nicoles-MacBook-Pro.local>
2018-11-12 20:34:54 -0500

    new

---
+++ js/jq.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+     $('h1').mouseenter(function(){
+        $('h1').addClass('bigger');
+    })
+    $('h1').mouseleave(function(){
+        $('h1').removeClass('bigger');
+        
+    })
+
+    
+     $('.periwinkle').mouseenter(function(){
+    	$('.periwinkle').addClass('black');
+    })
+    $('.periwinkle').mouseleave(function(){
+    	$('.periwinkle').removeClass('black');
+        
+    })
+  
+        
+});
+	   $('.peach').mouseenter(function(){
+    	$('.peach').addClass('black');
+    })
+    $('.peach').mouseleave(function(){
+    	$('.peach').removeClass('black');
+    })
+       $('.powderblue').mouseenter(function(){
+        $('.powderblue').addClass('black');
+    })
+    $('.powderblue').mouseleave(function(){
+        $('.powderblue').removeClass('black');
+    })
+       $('.mint').mouseenter(function(){
+        $('.mint').addClass('black');
+    })
+    $('.mint').mouseleave(function(){
+        $('.mint').removeClass('black');
+    })
+       $('.coral').mouseenter(function(){
+        $('.coral').addClass('black');
+    })
+    $('.coral').mouseleave(function(){
+        $('.coral').removeClass('black');
+    })
