e4f1410189a66141245099e15407b1d870e5049a
Nicole Starkey <coliestarkey@Nicoles-MacBook-Pro.local>
2018-12-05 19:38:29 -0500

    new

---
+++ js/gallery.js
@@ -0,0 +1,12 @@
+$('[data-fancybox="gallery"]').fancybox({
+	// Options will go here
+
+// Enable infinite gallery navigation
+  loop: false,
+
+  // Horizontal space between slides
+  gutter: 50,
+
+
+
+});
---
+++ js/homesmall.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+       $('.box2').mouseenter(function(){
+        $('box2').addClass('collection');
+    })
+    $('box2').mouseleave(function(){
+        $('').removeClass('bigger');
+        
+    })
+        
+    
+	
+	
